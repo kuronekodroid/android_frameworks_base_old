4021668f72e10f468c31fc1139dbaf7b5eda325b
pengzhicai <pengzhicai@xiaomi.com>
2018-08-06 09:55:24 +0800

    SystemUI: restart timeout causes black screen
    This change mainly delays the "scheduleServiceRestartLocked->
    mAm.Handler.postAtTime(r.restart,r.nextRestartTime);" startup
    service and puts it to "attachApplication Locked->
    mAm.mHandler.post(sr.restarter);".
    https://issuetracker.google.com/issues/111759613
    
    Test: systemui start time out
    
    Change-Id: Icb84b1cd3fe7692a21832510fa273816cf664156
    Signed-off-by: pengzhicai <pengzhicai@xiaomi.com>

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -5201,6 +5201,10 @@ public final class ActiveServices {
                 mPendingServices.remove(i);
                 size = mPendingServices.size();
                 i--;
+                if (proc.isPersistent() && !proc.isolated) {
+                    mRestartingServices.add(sr);
+                    continue;
+                }
                 needOomAdj = true;
                 bringDownServiceLocked(sr, true);
             }
