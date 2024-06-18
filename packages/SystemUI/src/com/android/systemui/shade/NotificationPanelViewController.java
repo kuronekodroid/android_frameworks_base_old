076a25b201d32ea463c80737659a2155e676ff37
danya2271 <dany2271@yandex.ru>
2024-06-18 16:07:32 +0500

    Revert "Temporary workaround to fix dt2s errors (it doesn't work for now)"
    This reverts commit 9dbadb5c68f64e59bdadcd3635e5bd7027beaa74.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4523,6 +4523,11 @@ public final class NotificationPanelViewController implements Dumpable {
 
         @Override
         public void onTuningChanged(String key, String newValue) {
+            if (DOUBLE_TAP_SLEEP_GESTURE.equals(key)) {
+                mDoubleTapToSleepEnabled = TunerService.parseIntegerSwitch(newValue,
+                        mResources.getBoolean(org.lineageos.platform.internal.R.bool.
+                                config_dt2sGestureEnabledByDefault));
+            }
         }
     }
 
