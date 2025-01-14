9566a594389d386c72b4067e255de53945766ca1
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-14 15:43:34 +0800

    Further reduce notifications bitmap resource usage
    * we're having qs panel lags when too many notifications with large bitmaps shows up (like youtube video preview), try compressing the notification bitmaps to webp to reduce memory usage by 40-60% assuming the bitmap is using jpg compression format
    
    Change-Id: Ie3c98a09abe5ec75081752c78d30ba6ac879c329

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -6856,7 +6856,7 @@ public class Notification implements Parcelable
         if (extras.getBoolean(EXTRA_REDUCED_IMAGES)) {
             return;
         }
-        boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+        boolean isLowRam = true;
 
         if (mSmallIcon != null
                 // Only bitmap icons can be downscaled.
@@ -7486,7 +7486,7 @@ public class Notification implements Parcelable
         public void reduceImageSizes(Context context) {
             super.reduceImageSizes(context);
             Resources resources = context.getResources();
-            boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+            boolean isLowRam = true;
             if (mPictureIcon != null) {
                 int maxPictureWidth = resources.getDimensionPixelSize(isLowRam
                         ? R.dimen.notification_big_picture_max_height_low_ram
@@ -8533,7 +8533,7 @@ public class Notification implements Parcelable
         public void reduceImageSizes(Context context) {
             super.reduceImageSizes(context);
             Resources resources = context.getResources();
-            boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+            boolean isLowRam = true;
             if (mShortcutIcon != null) {
                 int maxSize = resources.getDimensionPixelSize(
                         isLowRam ? R.dimen.notification_small_icon_size_low_ram
@@ -9580,9 +9580,7 @@ public class Notification implements Parcelable
             super.reduceImageSizes(context);
             if (mVerificationIcon != null) {
                 int rightIconSize = context.getResources().getDimensionPixelSize(
-                        ActivityManager.isLowRamDeviceStatic()
-                                ? R.dimen.notification_right_icon_size_low_ram
-                                : R.dimen.notification_right_icon_size);
+                        R.dimen.notification_right_icon_size_low_ram);
                 mVerificationIcon.scaleDownIfNecessary(rightIconSize, rightIconSize);
             }
         }
